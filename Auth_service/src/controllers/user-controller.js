f1b0145fb70b342619b2281814d987f847c71a6c
jsndz <jaisondz9360@gmail.com>
2025-08-31 23:19:56 +0530

    auth fixes

--- Auth_service/src/controllers/user-controller.js
+++ Auth_service/src/controllers/user-controller.js
@@ -1,107 +1,95 @@
-const UserService = require('../service/user-service');
-
-const {response} = require('express');
+const UserService = require("../service/user-service");
 
 const userService = new UserService();
 
-const create = async (req,res) => {
-    try {
-        const response = await userService.create({
-            email:req.body.email,
-            password:req.body.password
-        });
-        return res.status(201).json({
-            data: response,
-            error:{},
-            sucess:true,
-            message:"created the User"
-        })
-    } catch (error) {
-        // console.log(error);
-        return res.status(error.statusCode).json({
-            data: {},
-            err: error.explanation,
-            sucess:false,
-            message:error.message,
-            
-        })
-    }
-}
+const create = async (req, res) => {
+  try {
+    const response = await userService.create({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    return res.status(201).json({
+      data: response,
+      error: {},
+      sucess: true,
+      message: "created the User",
+    });
+  } catch (error) {
+    return res.status(error.statusCode).json({
+      data: {},
+      err: error.explanation,
+      sucess: false,
+      message: error.message,
+    });
+  }
+};
 
-const isAuthenticated = async (req,res) => {
-    try {
-        const token = await req.headers['x-access-token'];
-        const response = await userService.isAuthenticated(token);
-        return res.status(201).json({
-            data: response,
-            error:{},
-            sucess:true,
-            message:" authentication is successful"
-        })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            data: {},
-            error:{error},
-            sucess:false,
-            message:"authentication is unsuccessful"
-        })
-    }
-}
+const isAuthenticated = async (req, res) => {
+  try {
+    const token = await req.headers["x-access-token"];
+    const response = await userService.isAuthenticated(token);
+    return res.status(201).json({
+      data: response,
+      error: {},
+      sucess: true,
+      message: " authentication is successful",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      error: { error },
+      sucess: false,
+      message: "authentication is unsuccessful",
+    });
+  }
+};
 
-const signin = async (req,res) => {
-    try {
-        const response = await userService.signin(req.body.email,req.body.password);
-        return res.status(200).json({
-            data: response,
-            error:{},
-            sucess:true,
-            message:" sign in successful"
-        })
-    } catch (error) {
-        // console.log(error);
-        // console.log(error.statusCode)
-        // return res.status(error.statusCode).json({
-        //     data: {},
-        //     err: error.explanation,
-        //     sucess:false,
-        //     message:error.message
-        // })
-        return res.status(500).json({
-            data: {},
-            error:{error},
-            sucess:false,
-            message:"signin is unsuccessful"
-        })
-    }
-   
+const signin = async (req, res) => {
+  try {
+    const response = await userService.signin(
+      req.body.email,
+      req.body.password
+    );
+    return res.status(200).json({
+      data: response,
+      error: {},
+      sucess: true,
+      message: " sign in successful",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      data: {},
+      error: { error },
+      sucess: false,
+      message: "signin is unsuccessful",
+    });
+  }
+};
 
-}
-
-const isAdmin = async (req,res) => {
-    try {
-        
-        const response = await userService.isAdmin(req.body.id);
-        return res.status(201).json({
-            data: response,
-            error:{},
-            sucess:true,
-            message:"  is admin"
-        })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            data: {},
-            error:{error},
-            sucess:false,
-            message:"isnt admin"
-        })
-    }
-}
+const isAdmin = async (req, res) => {
+  try {
+    const response = await userService.isAdmin(req.body.id);
+    return res.status(201).json({
+      data: response,
+      error: {},
+      sucess: true,
+      message: "  is admin",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      error: { error },
+      sucess: false,
+      message: "isnt admin",
+    });
+  }
+};
 
 module.exports = {
-    create,
-    signin,
-    isAuthenticated,
-    isAdmin
-}
+  create,
+  signin,
+  isAuthenticated,
+  isAdmin,
+};
--- Auth_service/src/middlewares/index.js
+++ Auth_service/src/middlewares/index.js
@@ -1,3 +1,3 @@
-module.exports ={
-    AuthRequestValidators : require('./auth-request-validators')
-}
+module.exports = {
+  AuthRequestValidators: require("./auth-request-validators"),
+};
